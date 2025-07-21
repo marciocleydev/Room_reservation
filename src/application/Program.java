002b50d4b40c1d91b94c3fa4e54ef08e8dc1647e
Marcio cley <marciocleydev@gmail.com>
2025-07-21 18:15:04 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Rent[] rooms = new Rent[10];
+        System.out.print("how many rooms will be rented: ");
+        int n = sc.nextInt();
+        System.out.println();
+
+        for(int i = 0; i < n; i++){
+            sc.nextLine();
+            System.out.println("Rent #" + (i+1) + ":");
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+            rooms[room] = new Rent(name, email, room);
+            System.out.println();
+        }
+        System.out.println("Busy rooms: ");
+
+        for (int i = 0; i < rooms.length; i++){
+            if (rooms[i] != null){
+                System.out.println(rooms[i]);
+            }
+        }
+
+
+        sc.close();
+    }
+}
---
+++ src/entities/Rent.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Rent {
+    String name;
+    String email;
+    int roomNumber;
+
+    public Rent() {
+    }
+
+    public Rent(String name, String email, int roomNumber) {
+        this.name = name;
+        this.email = email;
+        this.roomNumber = roomNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return roomNumber
+                + ":  "
+                + name
+                + ",  "
+                + email;
+    }
+}
